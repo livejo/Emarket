ac6ae8c714916c848d775ac3c3d79939b7c989cf
yosef kefale <yoseph.kefale@gmail.com>
2021-11-14 17:49:18 +0300

    contact tab added and features have beem updated

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -8,11 +8,12 @@ import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 import { BreadcrumbModule } from 'xng-breadcrumb';
+import { ContactComponent } from './contact/contact.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent],
+  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent, ContactComponent],
   imports: [
     CommonModule,
     RouterModule,
